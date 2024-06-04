6aa0820cbbb13b3c59b9605b4463200522b4c992
Mephisto Lin <Mephisto007linlin@outlook.com>
2024-06-04 15:32:32 +0800

    feat: division and multiplication features done

--- py/test_money.py
+++ py/test_money.py
@@ -1,19 +1,38 @@
 import unittest  # 匯入 TestCase (superclass) 所需要的 unittest 套件
 
 
-class Dollar:
-    def __init__(self, amount):
+class Money:
+    def __init__(self, amount, currency):
         self.amount = amount
+        self.currency = currency
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Money(self.amount * multiplier, self.currency)
+
+    def divide(self, divisor):
+        return Money(self.amount / divisor, self.currency)
+
+    def __eq__(self, other):
+        return self.amount == other.amount and self.currency == other.currency
 
 
 class TestMoney(unittest.TestCase):  # 測試類別，必須是 unittest.TestCase 類別的子類別
-    def testMultiplication(self):  # 方法名稱必須以 test 開頭
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEqual(10, tenner.amount)
+    def testMultiplicationInDollars(self):  # 方法名稱必須以 test 開頭
+        fiveDollars = Money(5, "USD")
+        tenDollars = fiveDollars.times(2)
+        self.assertEqual(tenDollars, fiveDollars.times(2))
+
+    def testMultiplicationInEuros(self):
+        tenEuros = Money(10, "EUR")
+        twentyEuros = Money(20, "EUR")
+        self.assertEqual(twentyEuros, tenEuros.times(2))
+
+    def testDivision(self):
+        originalMoney = Money(4002, "KRW")
+        actualMoneyAfterDivision = originalMoney.divide(4)
+        expectedMoneyAfterDivision = Money(1000.5, "KRW")
+        self.assertEqual(expectedMoneyAfterDivision.amount,
+                         actualMoneyAfterDivision.amount)
 
 
 if __name__ == '__main__':
