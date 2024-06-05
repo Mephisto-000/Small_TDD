340ca3e209905a09c321dd9ccda15bf0b8be8219
Mephisto Lin <Mephisto007linlin@outlook.com>
2024-06-06 07:11:26 +0800

    feat: addition feature for Moneys in the same currency done

--- py/test_money.py
+++ py/test_money.py
@@ -1,4 +1,6 @@
 import unittest  # 匯入 TestCase (superclass) 所需要的 unittest 套件
+import functools  # 提供 reduce 函數
+import operator  # 提供 add 函數
 
 
 class Money:
@@ -14,6 +16,19 @@ class Money:
 
     def __eq__(self, other):
         return self.amount == other.amount and self.currency == other.currency
+
+
+class Portfolio:
+    def __init__(self):
+        self.moneys = []
+
+    def add(self, *moneys):
+        self.moneys.extend(moneys)
+
+    def evaluate(self, currency):
+        total = functools.reduce(
+            operator.add, map(lambda m: m.amount, self.moneys), 0)  # 增加累加結果的初始值
+        return Money(total, currency)
 
 
 class TestMoney(unittest.TestCase):  # 測試類別，必須是 unittest.TestCase 類別的子類別
@@ -33,6 +48,14 @@ class TestMoney(unittest.TestCase):  # 測試類別，必須是 unittest.TestCas
         expectedMoneyAfterDivision = Money(1000.5, "KRW")
         self.assertEqual(expectedMoneyAfterDivision.amount,
                          actualMoneyAfterDivision.amount)
+
+    def testAddition(self):
+        fiveDollars = Money(5, "USD")
+        tenDollars = Money(10, "USD")
+        fifteenDollars = Money(15, "USD")
+        portfolio = Portfolio()  # 宣告一個 Portfolio 物件
+        portfolio.add(fiveDollars, tenDollars)  # 向此 Portfolio 物件添加多個具有相同貨幣的 Money 物件
+        self.assertEqual(fifteenDollars, portfolio.evaluate("USD"))  # 以相同貨幣來評估 Portfolio
 
 
 if __name__ == '__main__':
